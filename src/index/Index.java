4bb16f9ca2bf318cdc825db870b98670a17a682f
Carlos Júnior <carlos.2018130435@unicap.br>
2022-09-16 20:22:54 -0300

    organizacao_final

--- src/index/Index.java
+++ src/index/Index.java
@@ -1,4 +1,5 @@
-
+package index;
+// package Index ;
 
 import java.util.Scanner;
 import java.io.FileNotFoundException;
@@ -52,7 +53,7 @@ public class Index {
     public void getEntryDegree() {
         int conte = 0;
         for (int i = 0; i < this.Matriz.length; i++) {
-            System.out.print("Entry degree of '" + i + "': ");
+            System.out.print("Entrada " + i + "= ");
             for (int j = 0; j < this.Matriz.length; j++) {
                 if (this.Matriz[j][i] != 0)
                     conte++;
@@ -72,38 +73,5 @@ public class Index {
     }
     public int getEdgesQuantity() {
         return quantidade;
-    }
-    public static void main(String[] args) throws Exception {
-        Index Index;
-        int[][] adjacencyMatrix;
-        int edgesQuantity, verticesQuantity, isolatedVertices;
-        Index = new Index(readFile());
-        adjacencyMatrix = Index.getAdjacencyMatrix();
-        edgesQuantity = Index.getEdgesQuantity();
-        verticesQuantity = Index.getVerticesQuantity();
-        isolatedVertices = Index.getIsolatedVertices();
-        System.out.println();
-        showAdjacencyMatrix(adjacencyMatrix);
-        System.out.println();
-        System.out.println("Ordem do gráfico: " + edgesQuantity);
-        System.out.println("Tamanho do gráfico: " + verticesQuantity);
-        System.out.println("Grau de entrada de todos os vértices: ");
-        Index.getEntryDegree();
-        System.out.println();
-        System.out.println("Número de vértices isolados: " + isolatedVertices);
-        System.out.println("Em um grafo direcionado, o número de vértices das extremidades é nulo");
-    }
-    private static void showAdjacencyMatrix(int[][] adjacencyMatrix) {
-        for (int i = 0; i < adjacencyMatrix.length; i++) {
-            for (int j = 0; j < adjacencyMatrix[0].length; j++) {
-                System.out.print(adjacencyMatrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-    private static Scanner readFile() throws FileNotFoundException {
-        String seach = "pequenoG.txt";
-        FileReader reader = new FileReader(seach);
-        return new Scanner(reader).useDelimiter("\\n");
     }
 }
